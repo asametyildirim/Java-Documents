223250776bc96f9ec20558947032dddc82f97cb7
AbdulSametYildirim <sametyldrym2@gmail.com>
2022-07-23 19:56:03 +0300

    Loop demo added
    Loop demo added

---
+++ loop_demo.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //for
+	for(int i=0;i<=10;i=i+2){
+	    System.out.println(i);
+
+    }
+        System.out.println("c");
+
+
+	//while
+    int i = 0;
+
+    while (i<10){
+        System.out.println(i);
+    i++;
+    }
+        System.out.println("Loop Over");
+
+    int j = 0;
+    //do while
+        do{
+            System.out.println(j);
+            j++;
+        }
+        while(j<=10);
+    }
+}
