115bfa4038934db0102a1e22bff7eff2d90da9e5
AbdulSametYildirim <sametyldrym2@gmail.com>
2022-07-31 19:33:21 +0300

    country add

---
+++ jbc_demo/country.java
@@ -0,0 +1,10 @@
+public class Country {
+    private String code,name,continent,region;
+
+    public  Country(String code, String name,String continent,String region){
+        this.code = code;
+        this.continent = continent;
+        this.name=name;
+        this.region=region;
+    }
+}
